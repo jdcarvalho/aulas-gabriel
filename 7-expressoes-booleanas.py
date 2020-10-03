3cea6362839242bbdfb697d7efbba228a86352bd
jcarvalho <joao.carvalho@maestrus.com>
2020-10-03 11:55:14 -0300

    Termino da aprendizagem dia 03/10/2010

--- 7-expressoes-booleanas.py
+++ 7-expressoes-booleanas.py
@@ -1,1 +1,34 @@
 #!/usr/bin/python3
+
+versao = 2
+
+miguel = {
+    'nome': 'Miguel',
+    'idade': 13,
+    'sabe_programar': False,
+    'sabe_jogar': True,
+    'joga_agario': True,
+    'joga_cs': 'Ruuuuuim',
+}
+
+gabriel = {
+    'nome': 'Gabriel',
+    'idade': 13,
+    'sabe_programar': False,
+    'sabe_jogar': True,
+    'joga_agario': False,
+    'joga_cs': 'Nada haver',
+}
+
+if versao == 1:
+    if ((miguel['sabe_jogar'] and gabriel['sabe_programar']) or (miguel['idade'] > 12 and gabriel['idade'] == 13)):
+        print('Nada haver irmão!')
+    else:
+        print('Ta tudo errado essas pergunta...')
+elif versao == 2:
+    if ((miguel['joga_cs'] or gabriel['joga_cs']) and (miguel['joga_agario'] or (miguel['sabe_jogar'] and miguel['sabe_programar']))):
+        print('Nada haver irmão!')
+    else:
+        print('Ta tudo errado essas pergunta...')
+else:
+    print('Ta tudo errado esse programa ai...')
---
+++ 8-operadores.py
@@ -0,0 +1,51 @@
+#!/usr/bin/python3
+# Operadores Unários
+valor = 1
+# valor +=1 == valor = valor + 1
+valor += 1
+valor -= 1
+print('\n----------------------Operadores Unários----------------------')
+print(valor)
+print('\n----------------------Operadores Binários----------------------')
+# Operadores Binários
+numero1 = 1
+numero2 = 2
+print(numero2 + numero1)
+print(numero1 - numero2)
+# Operadores Ternários
+print('\n----------------------Operadores Ternários----------------------')
+idade = 18
+maioridade = True if idade >= 18 else False
+print(maioridade)
+print('\n----------------------Operadores Relacionais----------------------')
+idade = 13
+maioridade = 18
+print('idade > maioridade: {0}'.format(idade > maioridade))
+print('idade < maioridade: {0}'.format(idade < maioridade))
+print('idade >= maioridade: {0}'.format(idade >= maioridade))
+print('idade <= maioridade: {0}'.format(idade < maioridade))
+print('idade == maioridade: {0}'.format(idade == maioridade))
+
+nome = 'Miguel'
+sobrenome = 'Carvalho'
+print(nome +' '+sobrenome)
+
+print('\n----------------------Índices de Lista----------------------')
+lista = [1,2,3,4,5,6,7,8,9,10]
+
+print('Tamanho da Lista: ', len(lista))
+print('Primeiro elemento da lista: ', lista[0])
+print('Último elemento da lista: ', lista[len(lista)-1])
+print('Último elemento da lista: ', lista[-1])
+
+print('\n----------------------Dicionários----------------------')
+valor = {
+    'nome': 'Gabriel',
+    'telefone': 'Quebrado',
+}
+valor.update({
+    'idade': 13,
+    'nome_do_pai': 'Silvio',
+})
+valor['nascimento'] = '06/12/2007'
+print(valor)
