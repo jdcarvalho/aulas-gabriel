c75dc6a092dad61704cb747d44cedc02d6347b12
jcarvalho <joao.carvalho@maestrus.com>
2020-10-03 10:11:42 -0300

    Lição 5. Melhor documentaçãp

--- 5-armazenamento-volatil.py
+++ 5-armazenamento-volatil.py
@@ -6,7 +6,13 @@ import math
 x = ((2 + 2) * 4 + 50) * math.sqrt(64)
 print(x)
 
+# Coerção de tipo
+# O resultado de X é um número real (float)
+# Para inserí-lo no arquivo de texto é necessário
+# realizar coerção de tipo e transformá-lo em string
+conteudo = str(x)
+
 # Memória não volátil
 with open('arquivo.txt', 'w') as f:
-    f.write(str(x))
+    f.write(conteudo)
     f.close()
